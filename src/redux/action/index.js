8383f37b9bfda52a7a572fa1f747dd19090dce51
chimenem Okandu <chimenemwillis@gmail.com>
2023-04-29 02:37:10 +0100

    all changes

---
+++ src/redux/action/index.js
@@ -0,0 +1,16 @@
+// for Add item to cart 
+export const addCart = (product)=>{
+    return{
+        type:"ADDITEM",
+        payload:product
+    }
+}
+
+
+// for Delete item to cart 
+export const delCart = (product)=>{
+    return{
+        type:"DELITEM",
+        payload:product
+    }
+}
